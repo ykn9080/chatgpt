f1d9fc07e1d9f749d5d7b1d5495e977ad4648c95
young <ykn9080@empal.com>
2023-12-04 12:01:18 +0900

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+import OpenAI from "openai";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const assistant = await openai.beta.assistants.create({
+  name: "Economics Tutor",
+  instructions: "This assistant will help you understand economics.",
+  tools: [{ type: "code_interpreter" }],
+  model: "gpt-3.5-turbo-1106",
+});
+
+// async function main() {
+//     const stream = await openai.chat.completions.create({
+//         model: "gpt-4",
+//         messages: [{ role: "user", content: "Say this is a test" }],
+//         stream: true,
+//     });
+//     for await (const chunk of stream) {
+//         process.stdout.write(chunk.choices[0]?.delta?.content || "");
+//     }
+// }
+
+// main();
